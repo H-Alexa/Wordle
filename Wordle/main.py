6044e1d6cbd67aa55e619b0a643a388f78eff624
H-Alexa <tatineesarkersunom@gmail.com>
2023-05-03 15:27:34 +0600

    added welcome screen, os independant clear screen and implemented green checks on characters on right location

--- Wordle/main.py
+++ Wordle/main.py
@@ -1,12 +1,32 @@
 import wordle
+import os
 
+os.system("cls") if os.name == "nt" else os.system("clear")
+
+begin_message = """
+'##:::::'##::'#######::'########::'########::'##:::::::'########:
+ ##:'##: ##:'##.... ##: ##.... ##: ##.... ##: ##::::::: ##.....::
+ ##: ##: ##: ##:::: ##: ##:::: ##: ##:::: ##: ##::::::: ##:::::::
+ ##: ##: ##: ##:::: ##: ########:: ##:::: ##: ##::::::: ######:::
+ ##: ##: ##: ##:::: ##: ##.. ##::: ##:::: ##: ##::::::: ##...::::
+ ##: ##: ##: ##:::: ##: ##::. ##:: ##:::: ##: ##::::::: ##:::::::
+. ###. ###::. #######:: ##:::. ##: ########:: ########: ########:
+:...::...::::.......:::..:::::..::........:::........::........::
+"""
+begin_message = begin_message.replace("#", f"{wordle.Color.GREEN}O{wordle.Color.BASE}")
+#begin_message = begin_message.replace(".", f"{wordle.Color.YELLOW}.{wordle.Color.BASE}")
+print(begin_message)
+print("Input a 5 letter word (in lowercase) to begin \n")
 if __name__ == '__main__':
+    with open("cheat.txt","w") as f:
+        f.write(wordle.CHOSEN)
     while True:
         guessIt = wordle.Guess(
-            w_str=input(">Input a 5 letter word\n>")
+            w_str=input(">")
         )
         if guessIt.isValid():
+            guessIt.applyGuess()
             guessIt.increment()
-            print(wordle.Guess.counter)
+            #print(wordle.Guess.counter)
         else:
             print("Invalid word, try again")
--- Wordle/wordle.py
+++ Wordle/wordle.py
@@ -4,13 +4,36 @@ from validWords import valid
 CHOSEN = random.choice(valid)
 CHANCES = 7
 
+class Color:
+    PREFIX = '\033'
+    BASE = "\033[0m"
+    GREY = "\033[90m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
 class Guess:
     counter = 1
     def __init__(self,w_str:str):
         self.w_str = w_str
         self.w_chars = list(self.w_str)
+        self.post_guess_w_str = ""
 
     def increment(self):
         Guess.counter += 1
     def isValid(self):
         return self.w_str in valid
+
+
+    def applyGreens(self):
+        for i,_ in enumerate(self.w_chars):
+            actualChar = CHOSEN[i]
+            guessedChar = self.w_chars[i]
+            if actualChar == guessedChar:
+                colored =  f"{Color.GREEN}{actualChar}{Color.BASE}"
+                self.w_chars[i] = colored
+    
+    def applyGuess(self):
+        self.applyGreens()
+        self.post_guess_w_str="".join(self.w_chars)
+        print(self.post_guess_w_str)
+
